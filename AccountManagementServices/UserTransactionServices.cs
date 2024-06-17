80693ffdcb7f80ed8cb8cde9c28bd445946b79fe
indaleen <inquinsayas@gmail.com>
2024-06-17 08:31:35 +0800

    refactor transaction services

--- AccountManagementServices/UserTransactionServices.cs
+++ AccountManagementServices/UserTransactionServices.cs
@@ -10,28 +10,38 @@ namespace AccountManagementServices
 
         public bool CreateUser(User user)
         {
-            bool result = validationServices.CheckIfUserNameExists(user.username);
+            bool result = false;
 
-            if (result)
+            if (validationServices.CheckIfUserNameExists(user.username))
             {
-                userData.AddUser(user);
-            }
-
-            return result ;
-        }
-
-        public bool UpdateUser(User user)
-        {
-            bool result = validationServices.CheckIfUserNameExists(user.username);
-
-            if (result)
-            {
-                userData.UpdateUser(user);
+                result = userData.AddUser(user) > 0;
             }
 
             return result;
         }
 
+        public bool UpdateUser(User user)
+        {
+            bool result = false;
 
+            if (validationServices.CheckIfUserNameExists(user.username))
+            {
+                result = userData.UpdateUser(user) > 0;
+            }
+
+            return result;
+        }
+
+        public bool DeleteUser(User user)
+        {
+            bool result = false;
+
+            if (validationServices.CheckIfUserNameExists(user.username))
+            {
+                result = userData.DeleteUser(user) > 0;
+            }
+
+            return result;
+        }
     }
 }
