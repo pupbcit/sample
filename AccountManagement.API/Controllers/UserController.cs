2d5f5ceaeecd693b7542ed4328f582d7e9302b6b
indaleen <inquinsayas@gmail.com>
2024-06-18 20:39:29 +0800

    Added API project

---
+++ AccountManagement.API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿
+using AccountManagementServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace AccountManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        UserGetServices _userGetServices;
+
+        public UserController()
+        {
+            _userGetServices = new UserGetServices();
+        }
+
+        [HttpGet]
+        public IEnumerable<AccountManagement.API.User> GetUsers()
+        {
+            var activeusers = _userGetServices.GetUsersByStatus(1);
+
+            List<AccountManagement.API.User> users = new List<User>();
+
+            foreach (var item in activeusers)
+            {
+                users.Add(new API.User { username = item.username, password = item.password, status = item.status });
+            }
+
+            return users;
+        }
+    }
+}
---
+++ AccountManagement.API/User.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountManagement.API
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public int status { get; set; }
+    }
+}
